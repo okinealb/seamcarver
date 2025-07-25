ecb0af8f9d6b5f78225612bbce82c36134886c24
Albert-Kenneth <albertjr329@gmail.com>
2025-07-25 13:27:33 -0500

    Refactoring SeamCalculator Add class call method for simpler usage. SeamCalculator now returns boolean mask by default; use mask_to_index on output for list of indices. Preparing for batching...

--- seamcarver/calculator.py
+++ seamcarver/calculator.py
@@ -36,12 +36,32 @@ class SeamCalculator:
 
     def __init__(
         self,
-        image: np.ndarray,
         method: EnergyMethod = GradientEnergy()
     ):
         """Initialize the SeamCalculator with an energy method."""
-        self.image = image
         self.method = method
+        
+        
+    def __call__(
+        self,
+        image: np.ndarray,
+        seams: int = 1,
+        batched: bool = False,
+    ) -> np.ndarray:
+        
+        """Find seams in the given image as a boolean mask."""
+    
+        # Get a copy of the image for processing
+        self.image = image.copy()
+        
+        if batched:
+            # TODO: Find batch size
+            # TODO: Updating energy table intermediately while computing seams
+            raise NotImplementedError("Batch seam calculation not implemented yet.")
+        else:
+            # TODO: Call regular single seam calculation
+            raise NotImplementedError("Single seam calculation not implemented yet.")
+        
         
     def find_seam(self, image: np.ndarray | None = None) -> np.ndarray:
         """Find and return a directional seam as a list of indices."""
@@ -56,6 +76,12 @@ class SeamCalculator:
         return self.seam
     
     
+    def mask_to_index(self, mask: np.ndarray) -> np.ndarray:
+        """Convert a boolean mask to a list of indices."""
+        # Get the indices where the mask is True
+        return np.argwhere(mask).flatten()
+    
+    
     def _compute_energy(self) -> None:
         """Compute the energy table for the image using the specified method.
     
@@ -65,6 +91,7 @@ class SeamCalculator:
         
         # Compute the energy table using the specified method
         self.energy_tbl = self.method.compute_energy(self.image)
+    
     
     def _compute_cost(self) -> None:
         """Compute cumulative minimum cost table using dynamic programming.
@@ -93,6 +120,7 @@ class SeamCalculator:
             # Compute the minimum cost for each pixel
             self.energy_cst[i] += np.minimum.reduce([prev, left, right])
     
+    
     def _compute_seam(self) -> None:
         """Backtrack through cost table to find minimum energy seam.
     
@@ -100,21 +128,27 @@ class SeamCalculator:
         upward, choosing the parent pixel that contributed to the minimum cost.
         """
         
-        # Initialize the seam array
-        self.seam = np.zeros(self.image.shape[0], dtype=np.uint16)
+        if self.seam is None:
+            # If not initialized, create a seam array
+            self.seam = np.zeros(self.image.shape[:2], dtype=bool)
+        
+        # Find the index of the minimum cost pixel in the last row
+        prev = np.argmin(self.energy_cst[-1], axis=0)
         
         # Set the last index of the seam to the minimum
-        self.seam[-1] = np.argmin(self.energy_cst[-1], axis=0)
+        self.seam[-1, prev] = True
+        self.energy_cst[-1, prev] = np.inf  # Mark as visited
 
         # Backtrack to find the seam path
         for i in range(self.image.shape[0]-2, -1, -1):
-            # Convert seam index to signed integer to avoid wrapping
-            seam_index = int(self.seam[i+1])
-            
             # Find the bounds for the left and right neighbors
-            left_bound = max(0, seam_index - 1)
-            right_bound = min(self.image.shape[1], seam_index + 2)
+            left_bound = np.max(0, prev - 1)
+            right_bound = np.min(self.image.shape[1], prev + 2)
             loc = np.argmin(self.energy_cst[i, left_bound:right_bound])
             
             # Update the seam index
             self.seam[i] = left_bound + loc
+            self.energy_cst[i, left_bound + loc] = np.inf
+            
+            # Update the previous column index for the next iteration
+            prev = left_bound + loc
